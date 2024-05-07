a5302baad5953ff69385f05df20719fa683fb262
MANTRAAAH <162032589+MANTRAAAH@users.noreply.github.com>
2024-05-07 16:57:39 +0200

    finito

---
+++ Settima Settimana/Giorno 2/script.js
@@ -0,0 +1,61 @@
+
+const removeButton = document.getElementById("remove_btn");
+const saveButton = document.getElementById("save_btn");
+const cleanButton = document.getElementById("clean_btn");
+const displayButton = document.getElementById("display_btn");
+const input = document.getElementById("name");
+
+const save = function () {
+    const inputField = document.getElementById("name");
+    const inputText = inputField.value;
+    localStorage.setItem("name", inputText);
+    console.log("Testo salvato", inputText);
+};
+
+const display_text = function () {
+    const displayDiv = document.getElementById("display_area");
+    const testoDaMostrare = localStorage.getItem("name");
+    displayDiv.innerHTML = testoDaMostrare;
+};
+
+const pulisci = function () {
+    const daPulire = document.getElementById("name");
+    daPulire.value = "";
+    console.log("Testo rimosso");
+}
+
+const remove = function () {
+    localStorage.removeItem("name");
+    console.log("Nome rimosso");
+};
+
+cleanButton.addEventListener("click", pulisci)
+
+saveButton.addEventListener("click", save)
+
+removeButton.addEventListener("click", remove);
+
+displayButton.addEventListener("click", display_text);
+
+
+
+
+//esercizio 2
+
+function updateCounter() {
+    if (sessionStorage.getItem('counter')) {
+        let counter = parseInt(sessionStorage.getItem('counter'));
+        counter++;
+        sessionStorage.setItem('counter', counter);
+        document.getElementById('counter').innerText = counter + " secondi";
+    } else {
+        sessionStorage.setItem('counter', 1);
+        document.getElementById('counter').innerText = 1 + " secondo";
+    }
+}
+
+function startCounter() {
+    setInterval(updateCounter, 1000);
+}
+
+window.onload = startCounter;
