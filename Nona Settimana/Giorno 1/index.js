fb39605a8849de918934439cc7a9abdd84867d53
MANTRAAAH <162032589+MANTRAAAH@users.noreply.github.com>
2024-05-20 14:56:59 +0200

    done

--- Nona Settimana/Giorno 1/index.js
+++ Nona Settimana/Giorno 1/index.js
@@ -1,4 +1,52 @@
 "use strict";
-function somma(a, b) {
-    return a + b;
+class User {
+    constructor(nome, cognome) {
+        this.credito = 0;
+        this.numeroChiamate = 0;
+        this.costoPerChiamata = 0.2; // Ad esempio, 0.2 euro al minuto
+        this.nome = nome;
+        this.cognome = cognome;
+    }
+    ricarica(importo) {
+        this.credito += importo;
+    }
+    chiamata(minuti) {
+        const costoChiamata = minuti * this.costoPerChiamata;
+        if (this.credito >= costoChiamata) {
+            this.credito -= costoChiamata;
+            this.numeroChiamate += minuti;
+        }
+        else {
+            console.log('Credito insufficiente per effettuare la chiamata');
+        }
+    }
+    chiamata404() {
+        return this.credito;
+    }
+    getNumeroChiamate() {
+        return this.numeroChiamate;
+    }
+    azzeraChiamate() {
+        this.numeroChiamate = 0;
+    }
 }
+// Crea nuovo utente
+let user = new User('Mario', 'Rossi');
+
+// Ricarica il credito dell'utente
+user.ricarica(20);
+
+// Effettua una chiamata di 5 minuti
+user.chiamata(5);
+
+// Stampa il credito rimanente
+console.log(user.chiamata404()); // Dovrebbe stampare 19
+
+// Stampa il numero di minuti in chiamata
+console.log(user.getNumeroChiamate()); // Dovrebbe stampare 5
+
+// Azzera il numero di minuti in chiamata
+user.azzeraChiamate();
+
+// Stampa il numero di minuti in chiamata dopo l'azzeramento
+console.log(user.getNumeroChiamate()); // Dovrebbe stampare 0
--- Nona Settimana/Giorno 1/index.ts
+++ Nona Settimana/Giorno 1/index.ts
@@ -1,3 +1,47 @@
-function somma(a:number,b:number){
-    return a+b;
+interface Smartphone {
+    credito: number;
+    numeroChiamate: number;
+    ricarica: (importo: number) => void;
+    chiamata: (minuti: number) => void;
+    chiamata404: () => number;
+    getNumeroChiamate: () => number;
+    azzeraChiamate: () => void;
+}
+
+class User implements Smartphone {
+    nome: string;
+    cognome: string;
+    credito: number = 0;
+    numeroChiamate: number = 0;
+    costoPerChiamata: number = 0.2; // Ad esempio, 0.2 euro al minuto
+
+    constructor(nome: string, cognome: string) {
+        this.nome = nome;
+        this.cognome = cognome;
+    }
+
+    ricarica(importo: number) {
+        this.credito += importo;
+    }
+
+chiamata(minuti: number) {
+    const costoChiamata = minuti * this.costoPerChiamata;
+    if (this.credito >= costoChiamata) {
+        this.credito -= costoChiamata;
+        this.numeroChiamate += minuti;
+    } else {
+        console.log('Credito insufficiente per effettuare la chiamata');
+    }
+}
+
+    chiamata404() {
+        return this.credito;
+    }
+    getNumeroChiamate() {
+        return this.numeroChiamate;
+    }
+
+    azzeraChiamate() {
+        this.numeroChiamate = 0;
+    }
 }
