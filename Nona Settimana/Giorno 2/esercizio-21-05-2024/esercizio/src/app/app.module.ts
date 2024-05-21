16c084e498fc5a2004c13bc4010d6f85868350d2
MANTRAAAH <162032589+MANTRAAAH@users.noreply.github.com>
2024-05-21 15:33:23 +0200

    esercizio angular 1

---
+++ Nona Settimana/Giorno 2/esercizio-21-05-2024/esercizio/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { Comp2Component } from './components/comp2/comp2.component';
+import { Comp3Component } from './components/comp3/comp3.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    Comp2Component,
+    Comp3Component
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
