d3307151fbcbe7a79b2dfdf035b8b97a1cfb53d5
MANTRAAAH <162032589+MANTRAAAH@users.noreply.github.com>
2024-05-22 15:50:39 +0200

    reworkd a bit

--- Nona Settimana/Giorno 3/esercizio/src/app/app.module.ts
+++ Nona Settimana/Giorno 3/esercizio/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HomeComponent } from './components/home/home.component';
 import { PostDetailComponent } from './components/post-detail/post-detail.component';
 import { ActivePostsComponent } from './components/active-posts/active-posts.component';
 import { InactivePostsComponent } from './components/inactive-posts/inactive-posts.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { InactivePostsComponent } from './components/inactive-posts/inactive-pos
     HomeComponent,
     PostDetailComponent,
     ActivePostsComponent,
-    InactivePostsComponent
+    InactivePostsComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
