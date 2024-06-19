9edca87117ce24befc117c6b50d88e7c7d230127
Annunziata Antonio Massimo <ant.annunz.02@gmail.com>
2024-06-19 15:00:37 +0200

    done

---
+++ Tredicesima Settimana/Giorno 3/Esercizio BackEnd S1L3/Esercizio BackEnd S1L3/ContoCorrente.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Esercizio_BackEnd_S1L3
+{
+    internal class ContoCorrente
+    {
+        private bool _isAttivo;
+        private decimal _saldo;
+
+        public ContoCorrente()
+        {
+            _isAttivo = false;
+            _saldo = 0;
+            
+        }
+
+        public void AttivaConto(decimal primoVersamento)
+        {
+            if (!_isAttivo && primoVersamento >= 1000)
+            {
+                _isAttivo = true;
+                _saldo += primoVersamento;
+                Console.WriteLine("Conto attivato con successo");
+            }
+            else
+            {
+                Console.WriteLine("Conto non attivato");
+            }
+
+        }
+
+        public void Versa(decimal importo)
+        {
+            if (_isAttivo)
+            {
+                _saldo += importo;
+                Console.WriteLine($"Versamento di {importo} euro effettuato con successo, il saldo è di {_saldo}");
+            }
+            else
+            {
+                Console.WriteLine("Conto non attivo");
+            }
+
+        }
+
+        public void Preleva(decimal importo)
+        {
+            if (_isAttivo && importo <= _saldo)
+            {
+                _saldo -= importo;
+                Console.WriteLine($"Prelevamento di {importo} euro effettuato con successo, il saldo è di {_saldo}");
+            }
+            else if (!_isAttivo)
+            {
+                Console.WriteLine("Conto non attivo");
+            }
+            else
+            {
+                Console.WriteLine("Saldo insufficiente");
+            }
+        }
+        public void visualizzaSaldo()
+        {
+            Console.WriteLine($"Il saldo attuale è di {_saldo}");
+        }
+    }
+ 
+}
---
+++ Tredicesima Settimana/Giorno 3/Esercizio BackEnd S1L3/Esercizio BackEnd S1L3/Program.cs
@@ -0,0 +1,70 @@
+﻿namespace Esercizio_BackEnd_S1L3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ContoCorrente conto = new ContoCorrente();
+            conto.AttivaConto(1000);
+            conto.Versa(500);
+            conto.Preleva(200);
+            conto.Preleva(300);
+            conto.visualizzaSaldo();
+
+
+            string[]nomi = {"Mario", "Luigi", "Giovanni", "Paolo", "Francesco", "Sara", "Michela", "Chiara", "Pia", "Lorena"};
+            Console.WriteLine("ricerca un nome:");
+            string? nomeDaCercare = Console.ReadLine();
+            bool trovato = false;
+            foreach (string nome in nomi)
+            {
+                if (nome.Equals(nomeDaCercare,StringComparison.OrdinalIgnoreCase))
+                {
+                    trovato = true;
+                    break;
+                }
+            } if (trovato)
+            {
+                Console.WriteLine($"il nome {nomeDaCercare} è stato trovato,premi un tasto per continuare");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine($"il nome {nomeDaCercare} non è stato trovato,premi un tasto per continuare");
+                Console.ReadLine();
+            }
+
+
+            Console.WriteLine("Inserisci dimensione array");
+            if (!int.TryParse(Console.ReadLine(), out int input))
+            {
+                Console.WriteLine("Input non valido. Impostazione del valore di default a 10.");
+                input = 10; // Imposta un valore di default o gestisci l'errore come preferisci
+            }
+
+            // Console.WriteLine("Inserisci moltiplicazione");
+            // int moltiplicazione = int.Parse(Console.ReadLine());
+
+            int[] numeri = new int[input];
+
+            for (int i = 0; i < input; i++)
+            {
+                numeri[i] = i+1;
+            //  numeri[i] = i * moltiplicazione;
+                Console.WriteLine(numeri[i]);
+            }
+
+             void somma_media(int[] numeri)
+            {
+                int somma = 0;
+                for (int i = 0; i < numeri.Length; i++)
+                {
+                    somma += numeri[i];
+                }
+                Console.WriteLine($"La somma è: {somma}; la media è {somma/numeri.Length}.");
+            }
+
+            somma_media(numeri);
+        }
+    }
+}
