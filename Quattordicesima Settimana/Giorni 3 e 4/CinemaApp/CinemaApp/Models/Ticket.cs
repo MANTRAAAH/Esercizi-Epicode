154343ee003aea238d4d9790872bcb6164dcd4bc
Annunziata Antonio Massimo <ant.annunz.02@gmail.com>
2024-06-26 16:08:12 +0200

    start

---
+++ Quattordicesima Settimana/Giorni 3 e 4/CinemaApp/CinemaApp/Models/Ticket.cs
@@ -0,0 +1,10 @@
+﻿namespace CinemaApp.Models
+{
+    public class Ticket
+    {
+        public string Nome { get; set; }
+        public string Cognome { get; set; }
+        public string Sala { get; set; }
+        public string TipoBiglietto { get; set; }
+    }
+}
