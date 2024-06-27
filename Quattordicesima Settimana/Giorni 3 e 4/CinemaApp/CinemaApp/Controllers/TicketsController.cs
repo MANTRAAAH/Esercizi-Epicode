f8fca0aa1c3a95d120c23a5fbafa4ff69de72a46
Annunziata Antonio Massimo <ant.annunz.02@gmail.com>
2024-06-27 16:52:22 +0200

    26 & 27/06/2024 DONE

---
+++ Quattordicesima Settimana/Giorni 3 e 4/CinemaApp/CinemaApp/Controllers/TicketsController.cs
@@ -0,0 +1,57 @@
+﻿using CinemaApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CinemaApp.Controllers
+{
+
+    public class TicketsController : Controller
+    {
+        private static List<Ticket> tickets = new List<Ticket>();
+        private static Dictionary<string, int> salaCapienza = new Dictionary<string, int>
+        {
+            { "SALA NORD", 0 },
+            { "SALA EST", 0 },
+            { "SALA SUD", 0 }
+        };
+        private static Dictionary<string, int> salaRidotti = new Dictionary<string, int>
+        {
+            { "SALA NORD", 0 },
+            { "SALA EST", 0 },
+            { "SALA SUD", 0 }
+        };
+
+        private const int maxCapienza = 120;
+
+        public IActionResult Index()
+        {
+            ViewBag.Tickets = tickets;
+            ViewBag.SalaCapienza = salaCapienza;
+            ViewBag.SalaRidotti = salaRidotti;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Prenota(Ticket ticket)
+        {
+            if (salaCapienza[ticket.Sala] < maxCapienza)
+            {
+                tickets.Add(ticket);
+                salaCapienza[ticket.Sala]++;
+
+                if (ticket.TipoBiglietto == "Ridotto")
+                {
+                    salaRidotti[ticket.Sala]++;
+                }
+            }
+            else
+            {
+                TempData["Errore"] = "Capienza massima della sala raggiunta.";
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+}
+
---
+++ Quattordicesima Settimana/Giorni 3 e 4/CinemaApp/CinemaApp/Models/Ticket.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CinemaApp.Models
+{
+    public class Ticket
+    {
+        [Display(Name ="Nome:")]
+        public string Nome { get; set; }
+        [Display(Name = "Cognome:")]
+        public string Cognome { get; set; }
+        [Display(Name = "Scegli la sala:")]
+        public string Sala { get; set; }
+        [Display(Name = "Scegli tipo di biglietto:")]
+        public string TipoBiglietto { get; set; }
+    }
+}
