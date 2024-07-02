fcd2d19baee1891031a9a72c59bcbf4daec03766
Annunziata Antonio Massimo <ant.annunz.02@gmail.com>
2024-07-02 09:41:51 +0200

    02/07/2024 DONE

--- Quindicesima Settimana/Giorno 1/dbo.Cliente.sql
+++ Quindicesima Settimana/Giorno 1/dbo.Cliente.sql
@@ -3,9 +3,9 @@
     [Nome]        NVARCHAR (50) NOT NULL,
     [Cognome]     NVARCHAR (50) NOT NULL,
     [DataNascita] DATE          NOT NULL,
-    [Sesso]       CHAR (1)      NOT NULL,
-    [CF]          CHAR (16)     NOT NULL,
-    [P.IVA]       CHAR (11)     NOT NULL,
+    [Sesso]       CHAR (1)      NULL,
+    [CF]          CHAR (16)     NULL,
+    [P.IVA]       CHAR (11)     NULL,
     [Attivo]      BIT           NOT NULL,
     [Saldo]       MONEY         NULL,
     PRIMARY KEY CLUSTERED ([IdCliente] ASC)
---
+++ Quindicesima Settimana/Giorno 2/Esercizio.sql
@@ -0,0 +1,15 @@
+﻿SELECT * FROM Products
+
+Select * from Products where UnitsInStock > 40
+
+Select * from Employees where City = 'London'
+
+Select * from Orders order by Freight desc
+
+Select * from Orders where Freight > 90 AND Freight < 200 order by Freight asc
+
+Select * from Products where CategoryID= 1
+
+Select * from [Order Details] where Discount > 0
+
+Select * from Orders where CustomerID = 'BOTTM' and Freight > 50
